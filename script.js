f86468b462d4495cb6b96562b2d2e030b97463ee
Dawid <dawidgalsino@vp.pl>
2018-12-15 11:53:17 +0100

    beta version - added target media, cleaned up code

--- script.js
+++ script.js
@@ -11,38 +11,54 @@ var audioVisualWinners
 // Array of objects. Each object contains sound and picture used.
 var audioVisual = [
 	{	tag: generateTag(),
-		audio: new Audio("media/cat.mp3"),
-		visual: "media/cat.jpeg",
+		audio: new Audio("media/1_beben_4180ms.wav"),
+		visual: "media/1_beben_4180ms.jpg",
 		winner: false,
-		description: "cat"
+		description: "Drum"
 	},
 	{
 		tag: generateTag(),
-		audio: new Audio("media/cricket.mp3"),
-		visual: "media/cricket.jpeg",
+		audio: new Audio("media/2_grzechotka_czerwona_7375ms.wav"),
+		visual: "media/2_grzechotka_czerwona_7375ms.jpg",
 		winner: false,
-		description: "cricket"
+		description: "Rattle"
 	},
 	{
 		tag: generateTag(),
-		audio: new Audio("media/horse.mp3"),
-		visual: "media/horse.jpeg",
+		audio: new Audio("media/3_dzwonek_zolty_6606ms.wav"),
+		visual: "media/3_dzwonek_zolty_6606ms.jpg",
 		winner: false,
-		description: "horse"
+		description: "Bell"
 	},
+
 	{
 		tag: generateTag(),
-		audio: new Audio("media/truck.mp3"),
-		visual: "media/truck.jpeg",
+		audio: new Audio("media/5_trojkat2_7007ms.wav"),
+		visual: "media/5_trojkat2_7007ms.jpg",
 		winner: false,
-		description: "truck"
+		description: "Triangle"
 	},
+
 	{
 		tag: generateTag(),
-		audio: new Audio("media/dog.mp3"),
-		visual: "media/dog.jpeg",
+		audio: new Audio("media/6_tamburyno_stukanie1_4389ms.wav"),
+		visual: "media/6_tamburyno_stukanie1_4389ms.jpg",
 		winner: false,
-		description: "dog"
+		description: "Tambourine"
+	},
+		{
+		tag: generateTag(),
+		audio: new Audio("media/7_dzwoneczki_6860ms.wav"),
+		visual: "media/7_dzwoneczki_6860ms.jpg",
+		winner: false,
+		description: "Little bells"
+	},
+		{
+		tag: generateTag(),
+		audio: new Audio("media/8_drewienko_akustyczne_9894ms.wav"),
+		visual: "media/8_drewienko_akustyczne_9894ms.jpg",
+		winner: false,
+		description: "tWood"
 	}
 ]
 
